7b56931f950f58664e03bc0960b34788144eb14a
Zohaib2024 <162030191+Zohaib2024@users.noreply.github.com>
2024-03-17 16:29:31 +0500

    Add files via upload

---
+++ Exercise46.ts
@@ -0,0 +1,10 @@
+let Laptops = {
+
+  make: "dell",
+  model:"optiplex",
+  year: 2018,
+describe: function(){
+console.log("i have " + this.make + " " + this.model + " " + this.year)}
+}
+Laptops.describe();
+
