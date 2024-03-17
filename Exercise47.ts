d9f9f4afdd9a32b3887e703eaa1b07ecb14cc9fe
Zohaib2024 <162030191+Zohaib2024@users.noreply.github.com>
2024-03-17 23:50:15 +0500

    Add files via upload

---
+++ Exercise47.ts
@@ -0,0 +1,15 @@
+let laptops = [
+    {
+        make: "dell" , model: "optiplex" , year: "2018"
+    },
+    {
+        make: "Lenovo" , model: "Legion" , year: "2019"
+    },
+     {
+        make: "Mac" , model: "iMac" , year: "2022"
+    }
+];
+let [laptop1,laptop2,laptop3,laptop4] = laptops
+console.log(laptop1);
+console.log(laptop2);
+console.log(laptop3);
